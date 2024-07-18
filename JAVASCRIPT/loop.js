ac2be7c2da216da26fcdbd3b3ae2ef8932cce0bd
ommanirai <ommanirai27@gmail.com>
2024-07-18 09:24:44 +0545

    while and do while loop

--- JAVASCRIPT/loop.js
+++ JAVASCRIPT/loop.js
@@ -67,11 +67,36 @@ for(i=1;i<=10;i++){
 print(sum)
 
 2. while
-3. do while
-*/
-var n = 6
-for(i=1;i<=10;i++){
-    // i = 1 to 10
-    // console.log(i*n)
-    console.log(n+" x "+i+" = "+n*i)
+syntax:
+initialization
+while(condition){
+    body of while
+    increment/decrement
 }
+
+3. do while
+syntax:
+initialization
+do{
+    body of do while
+    increment/decrement
+}while(condition)
+
+*/
+// var n = 6
+// for(i=1;i<=10;i++){
+//     // i = 1 to 10
+//     // console.log(i*n)
+//     console.log(n+" x "+i+" = "+n*i)
+// }
+// 
+var i = 1
+// while(i<=10){
+//     console.log(i)
+//     i++
+// }
+do {
+    console.log(i)
+    i++
+} while (i <= 10)
+
